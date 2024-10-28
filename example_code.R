5c6b8b8fd5bb90dbcd3dcdd1b918894404bea2df
Jose Sanchez <jsanchez@ws-ls-cacau.igtpscp.local>
2024-10-28 17:08:12 +0100

    restructure the code

--- example_code.R
+++ example_code.R
@@ -2,13 +2,11 @@
 ## data test
 ##################################
 ## test file
-test_file <- "./dataExample/exampleCounts.csv"
 source("./draculR.HGCB/R/draculR_counts.R")
 source("./draculR.HGCB/R/draculR_validate.R")
 
-## Number in smallest group, default=1
+test_file <- "./dataExample/exampleCounts.csv"
 counts.test <- draculR_parse_file(raw_data = test_file, sep_input = ",", verbose = FALSE)
 draculR_results <- draculR_parse_counts(counts_df = counts.test)
-
 draculR_results
 
